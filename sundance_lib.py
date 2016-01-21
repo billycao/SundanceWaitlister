e059ef3c807e985bc4a985abfb5fd766b8c674b8
Billy Cao <the.billy.c@gmail.com>
2016-01-20 18:04:42 -0700

    Simple login and session handling.

--- sundance_lib.py
+++ sundance_lib.py
@@ -1,10 +1,10 @@
 import requests
 
-def SignUp(first_name,
-           last_name,
-           email,
-           zip_code,
-           password):
+def sign_up(first_name,
+            last_name,
+            email,
+            zip_code,
+            password):
   # Create POST request.
   payload = {
     'data[User][first_name]': first_name,
@@ -16,4 +16,21 @@ def SignUp(first_name,
     'data[User][agree]': 1,
   }
   r = requests.post('https://ewaitlist.sundance.org/register', data=payload)
+  # TODO(billycao): Add error handling here.
   return r.text
+
+
+class WaitlistSession(object):
+  def __init__(self):
+    self.session = requests.Session()
+      
+  def login(self, email, password):
+    payload = {
+      'data[User][username]': email,
+      'data[User][password]': password,
+    }
+    response = (
+        self.session.post('https://ewaitlist.sundance.org/login', data=payload))
+    if "Invalid email or password" in response.text:
+      return False
+    return True
--- sundance_main.py
+++ sundance_main.py
@@ -3,12 +3,18 @@ import sys
 import sundance_lib
 
 def main(argv):
-  print sundance_lib.SignUp(
-      'test_first',
-      'test_last',
-      'my_sundance_email@mailinator.com',
-      '91007',
-      '123456')
+  # TODO: Create multiple accounts, store into local gitignored json file.
+  # TODO: Do some error printing here.
+  # print sundance_lib.sign_up(
+  #     'test_first',
+  #     'test_last',
+  #     'my_sundance_email@mailinator.com',
+  #     '91007',
+  #     '123456')
+
+  my_session = sundance_lib.WaitlistSession()
+  if not my_session.login('my_sundance_email@mailinator.com', '123456'):
+    print "Invalid Login."
 
 if __name__ == '__main__':
   main(sys.argv)
